243cfd9d4e759127943f6d7c2fc1a41aab1c9598
Eteil Djoumatchoua (etdj) <eteil.djoumatchoua@gmail.com>
2022-09-29 16:11:11 +0200

    webforms

--- hello.py
+++ hello.py
@@ -1,29 +1,43 @@
 from flask import Flask, request, make_response, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 from datetime import datetime
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'djoumatchoua' # Mandatory for wtf modules
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(e): # Do not forget the error arg
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit(): # Verify if the form has been submitted
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 
 @app.route('/user/<string:name>')
 def get_user(name):
     return render_template('user.html', name=name)
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
