dbecba3d0a06979bb13d625ba86f0740928278dc
Eteil Djoumatchoua (etdj) <eteil.djoumatchoua@gmail.com>
2022-09-29 14:00:10 +0200

    templates

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
+from flask_bootstrap import Bootstrap
+
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1> Hello</h1>'
-    
+    return render_template('index.html')
+
+
+@app.route('/user/<string:name>')
+def get_user(name):
+    return render_template('user.html', name=name)
