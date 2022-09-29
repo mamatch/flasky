b78e85a8eb7bbff8d2d78dc70d552157e4d3f10f
Eteil Djoumatchoua (etdj) <eteil.djoumatchoua@gmail.com>
2022-09-29 14:34:16 +0200

    error handling

--- hello.py
+++ hello.py
@@ -5,6 +5,14 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,3 +21,5 @@ def index():
 @app.route('/user/<string:name>')
 def get_user(name):
     return render_template('user.html', name=name)
+
+
