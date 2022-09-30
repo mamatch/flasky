28e28c67c77d1ee3deeb27f059364ce69f703ade
Eteil Djoumatchoua (etdj) <eteil.djoumatchoua@gmail.com>
2022-09-30 14:45:30 +0200

    db setup

--- hello.py
+++ hello.py
@@ -2,16 +2,27 @@ from flask import Flask, request, make_response, render_template, session, redir
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 from datetime import datetime
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'djoumatchoua' # Mandatory for wtf modules
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+# ----------------------------------------- View functions ------------------------------------------------
 
 @app.errorhandler(404)
 def page_not_found(e): # Do not forget the error arg
@@ -38,8 +49,29 @@ def get_user(name):
     return render_template('user.html', name=name)
 
 
+# ----------------------------------- Forms -----------------------------------------------
+
 class NameForm(FlaskForm):
     name = StringField('What is your name', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
+# ----------------------------------- Models ----------------------------------------------
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True) # SqlAlchemy always needs an id
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role') # To repr one-to-many
+
+    def __repr__(self) -> str:
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role = db.Column(db.Integer, db.ForeignKey('role.id')) # To repr the many-to-one relation
+
+    def __repr__(self):
+        return '<User %r>' % self.username
