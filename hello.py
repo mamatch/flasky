1c5c688749429b1c3bb3d0da77390ea954d8fe50
Eteil Djoumatchoua (etdj) <eteil.djoumatchoua@gmail.com>
2022-09-30 16:01:56 +0200

    interact with db from view fufunctions

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -13,65 +13,89 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'djoumatchoua' # Mandatory for wtf modules
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SECRET_KEY"] = "djoumatchoua"  # Mandatory for wtf modules
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "data.sqlite"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 
 
-# ----------------------------------------- View functions ------------------------------------------------
+# ------------------- View functions --------------------
+
 
 @app.errorhandler(404)
-def page_not_found(e): # Do not forget the error arg
-    return render_template('404.html'), 404
+def page_not_found(e):  # Do not forget the error arg
+    return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
-    if form.validate_on_submit(): # Verify if the form has been submitted
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name')
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=session.get('name'))
+    if form.validate_on_submit():  # Verify if the form has been submitted
+        user = User.query.filter_by(username=form.name.data).first()
+        if not user:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session["known"] = False
+        else:
+            session["known"] = True
+        session["name"] = form.name.data
+        form.name.data = ""
+        return redirect(url_for("index"))
+    return render_template(
+        "index.html",
+        current_time=datetime.utcnow(),
+        form=form,
+        name=session.get("name"),
+        known=session.get("known", False),
+    )
 
 
-@app.route('/user/<string:name>')
+@app.route("/user/<string:name>")
 def get_user(name):
-    return render_template('user.html', name=name)
+    return render_template("user.html", name=name)
 
 
-# ----------------------------------- Forms -----------------------------------------------
+# ---------------------------- Forms -----------------------------------
+
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
-# ----------------------------------- Models ----------------------------------------------
+# ---------------------------- Models ----------------------------------
+
 
 class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True) # SqlAlchemy always needs an id
+    __tablename__ = "roles"
+    # SqlAlchemy always needs an id
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic') # To repr one-to-many
+    users = db.relationship(
+        "User", backref="role", lazy="dynamic"
+    )  # To repr one-to-many
 
     def __repr__(self) -> str:
-        return '<Role %r>' % self.name
+        return "<Role %r>" % self.name
+
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id')) # To repr the many-to-one relation
+    # To repr the many-to-one relation
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return "<User %r>" % self.username
